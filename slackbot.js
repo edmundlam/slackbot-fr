1246b4ae6947c7e4becaa16d008ad7540246d40c
Edmund <elam.txt@gmail.com>
2017-01-28 14:50:18 -0500

    Add initial slackbot code

--- slackbot.js
+++ slackbot.js
@@ -1,1 +1,23 @@
 var SlackBot = require('slackbots');
+var config = require('./config');
+
+console.log("Read config: " +  config)
+console.log(config)
+
+// Initialize the bot
+var bot = new SlackBot({
+    token: config.token,
+    name: config.name
+});
+
+
+bot.on('start', function() {
+    var params = {
+        icon_emoji: ':cat:'
+    };
+
+    bot.postMessageToUser('edmund', 'Bot started', params); 
+})
+
+
+module.exports = bot
