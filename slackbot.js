013e70113826de6f40dc81db358e96146e244d8a
Edmund <elam.txt@gmail.com>
2017-01-28 16:32:19 -0500

    Find channel/group id and shutdown on command

--- slackbot.js
+++ slackbot.js
@@ -14,25 +14,48 @@ var params = {
 };
 
 bot.on('start', function() {
-    bot.postMessageToGroup('test', 'Bot started', params);
+
+    bot.getChannelId(config.channelName).always(function(data) {
+        if (data._status == 2) {
+            config.channelId = data._value;
+            bot.postMessage(config.channelId, 'Bot started', params);
+        }
+        else {
+            bot.getGroupId(config.channelName).always(function(data) {
+                if (data._status == 2) {
+                    config.channelId = data._value;
+                    bot.postMessage(config.channelId, 'Bot started', params);
+                } else {
+                    console.log("Could not find id for channel " + config.channelName)
+                }
+            })
+        }
+    })
 })
 
 bot.on('message', function(data) {
     /**
     **/
 
-    if (data.type == 'message' && data.subtype != 'bot_message') {
+    if (data.type == 'message' &&
+        data.subtype != 'bot_message' &&
+        data.channel == config.channelId) {
 
-        var channel = bot.groups.filter(function (item) {
-            return item.id === data.channel
-        });
+            if (data.text === 'edbot shutdown') {
+                bot.postMessage(config.channelId,
+                                "Shutting myself down",
+                                params
+                                ).then(function() {
+                                    process.exit()
+                                })
+            }
 
-        if (channel.length > 0 && channel[0].name === config.channel) {
-            bot.postMessageToGroup(
-                config.channel, 
-                "Hello <@" + data.user + "> I got your message, it was: " + data.text, 
-                params);
-        }
+            else {
+                bot.postMessage(
+                    config.channelId,
+                    "Hello <@" + data.user + "> I got your message, it was: " + data.text,
+                    params);
+            }
     }
 
     console.log(JSON.stringify(data, null, 4))
