e346be7b16afd8a918897ca009869128359ece75
Edmund <elam.txt@gmail.com>
2017-01-28 14:22:48 -0500

    Add config.js example and update .gitignore

---
+++ config-example.js
@@ -0,0 +1,12 @@
+/**
+ * Example config file for the slackbot.
+ * Copy this file to config.js and set up your token
+ * before running the bot. 
+**/
+
+var config = {};
+
+config.token = 'Your token here'; // Add a bot https://my.slack.com/services/new/bot and put the token 
+config.name = 'My Bot'
+
+module.exports = config;
