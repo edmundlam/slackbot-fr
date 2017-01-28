ad6fd2efed05876bfd70accbdde95088827b1403
Edmund <elam.txt@gmail.com>
2017-01-28 15:39:47 -0500

    Make bot listen and respond in specified channel

--- slackbot.js
+++ slackbot.js
@@ -1,8 +1,7 @@
 var SlackBot = require('slackbots');
 var config = require('./config');
 
-console.log("Read config: " +  config)
-console.log(config)
+console.log("Read config: " +  JSON.stringify(config, null, 4))
 
 // Initialize the bot
 var bot = new SlackBot({
@@ -10,14 +9,33 @@ var bot = new SlackBot({
     name: config.name
 });
 
+var params = {
+        icon_emoji: ':cat:'
+};
 
 bot.on('start', function() {
-    var params = {
-        icon_emoji: ':cat:'
-    };
-
-    bot.postMessageToUser('edmund', 'Bot started', params); 
+    bot.postMessageToGroup('test', 'Bot started', params);
 })
 
+bot.on('message', function(data) {
+    /**
+    **/
+
+    if (data.type == 'message' && data.subtype != 'bot_message') {
+
+        var channel = bot.groups.filter(function (item) {
+            return item.id === data.channel
+        });
+
+        if (channel.length > 0 && channel[0].name === config.channel) {
+            bot.postMessageToGroup(
+                config.channel, 
+                "Hello <@" + data.user + "> I got your message, it was: " + data.text, 
+                params);
+        }
+    }
+
+    console.log(JSON.stringify(data, null, 4))
+})
 
 module.exports = bot
