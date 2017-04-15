12916378afe08f40440b0cd94e48b040cb105ece
Edmund <elam.txt@gmail.com>
2017-04-15 10:59:00 -0400

    Add game status and commands to start and stop game

--- slackbot.js
+++ slackbot.js
@@ -9,9 +9,15 @@ var bot = new SlackBot({
     name: config.name
 });
 
+// Parameters sent with messages
 var params = {
         icon_emoji: ':cat:'
 };
+
+// Game status
+var GameInfo = {
+    active: false
+}
 
 bot.on('start', function() {
 
@@ -34,31 +40,57 @@ bot.on('start', function() {
 })
 
 bot.on('message', function(data) {
-    /**
-    **/
+    /** Listen to incoming messages **/
 
     if (data.type == 'message' &&
         data.subtype != 'bot_message' &&
         data.channel == config.channelId) {
 
-            if (data.text === 'edbot shutdown') {
-                bot.postMessage(config.channelId,
-                                "Shutting myself down",
-                                params
-                                ).then(function() {
-                                    process.exit()
-                                })
+            if (data.text === 'edbot start quiz') {
+                start_quiz()
+            }
+
+            else if (data.text === 'edbot stop quiz') {
+                stop_quiz()
+            }
+
+            else if (data.text === 'edbot shutdown') {
+                post_message("Shutting myself down").then(function() {process.exit()})
+            }
+
+            else if (data.text == 'last message') {
+                post_message("Hello <@" + data.user + "> the last message was: " + history);
             }
 
             else {
-                bot.postMessage(
-                    config.channelId,
-                    "Hello <@" + data.user + "> I got your message, it was: " + data.text,
-                    params);
+                history = data.text
+                post_message("Hello <@" + data.user + "> I got your message, it was: " + data.text);
             }
     }
 
     console.log(JSON.stringify(data, null, 4))
 })
+
+start_quiz = function() {
+    if (GameInfo.active) {
+        post_message("Game already started!")
+    } else {
+        post_message("Starting quiz!")
+        GameInfo.active = true
+    }
+}
+
+stop_quiz = function() {
+    if (GameInfo.active) {
+        post_message("Stopping quiz")
+        GameInfo.active = false
+    } else {
+        post_message("No game to stop!")
+    }
+}
+
+post_message = function(message) {
+    return bot.postMessage(config.channelId, message, params);
+}
 
 module.exports = bot
